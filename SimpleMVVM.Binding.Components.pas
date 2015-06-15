a4606a7118cf07faf093244700abeac7e3388e78
Stefan Glienke <sglienke@dsharp.org>
2015-06-15 18:35:57 +0200

    bindings refactoring

--- SimpleMVVM.Binding.Components.pas
+++ SimpleMVVM.Binding.Components.pas
@@ -20,25 +20,29 @@ uses
 type
   TBinding = class(TComponent)
   private
-    fObservable: IObservable;
+    fSource: IObservable;
+    fTarget: TComponent;
   protected
-    procedure InitComponent; virtual;
-    procedure InitObservable(const observable: IObservable); virtual;
     function InitGetValue(const observable: IObservable): TFunc<TValue>; virtual;
-    procedure SetComponent(const component: TComponent); virtual; abstract;
+    procedure InitSource(const observable: IObservable); virtual;
+    procedure InitTarget; virtual;
+
+    property Source: IObservable read fSource;
+    property Target: TComponent read fTarget;
   public
-    constructor Create(const component: TComponent;
-      const observable: IObservable); reintroduce;
+    constructor Create(const target: TComponent;
+      const source: IObservable); reintroduce;
   end;
 
   TBindingClass = class of TBinding;
 
   TBinding<T: TComponent> = class(TBinding)
+  private
+    function GetTarget: T;
   protected
-    fComponent: T;
-    procedure SetComponent(const component: TComponent); override;
+    property Target: T read GetTarget;
   public
-    constructor Create(const component: T; const observable: IObservable);
+    constructor Create(const target: T; const source: IObservable);
   end;
 
   TComponentBinding = class(TBinding<TComponent>)
@@ -46,29 +50,29 @@ type
     fProperty: TRttiProperty;
     function InitGetValue(const observable: IObservable): TFunc<TValue>; override;
   public
-    constructor Create(const component: TComponent; const observable: IObservable;
+    constructor Create(const target: TComponent; const source: IObservable;
       const propertyName: string); reintroduce;
   end;
 
   TButtonBinding = class(TBinding<TButton>)
   protected
     procedure HandleClick(Sender: TObject);
-    procedure InitComponent; override;
-    procedure InitObservable(const observable: IObservable); override;
+    procedure InitTarget; override;
+    procedure InitSource(const observable: IObservable); override;
   end;
 
   TEditBinding = class(TBinding<TEdit>)
   protected
     procedure HandleChange(Sender: TObject);
-    procedure InitComponent; override;
     function InitGetValue(const observable: IObservable): TFunc<TValue>; override;
+    procedure InitTarget; override;
   end;
 
   TComboBoxBinding = class(TBinding<TComboBox>)
   protected
     procedure HandleChange(Sender: TObject);
-    procedure InitComponent; override;
     function InitGetValue(const observable: IObservable): TFunc<TValue>; override;
+    procedure InitTarget; override;
   end;
 
   TLabelBinding = class(TBinding<TLabel>)
@@ -128,17 +132,13 @@ end;
 
 {$REGION 'TBinding'}
 
-constructor TBinding.Create(const component: TComponent;
-  const observable: IObservable);
+constructor TBinding.Create(const target: TComponent;
+  const source: IObservable);
 begin
-  inherited Create(component);
-  SetComponent(component);
-  InitObservable(observable);
-  InitComponent;
-end;
-
-procedure TBinding.InitComponent;
-begin
+  inherited Create(target);
+  fTarget := target;
+  InitSource(source);
+  InitTarget;
 end;
 
 function TBinding.InitGetValue(const observable: IObservable): TFunc<TValue>;
@@ -150,9 +150,9 @@ begin
     end;
 end;
 
-procedure TBinding.InitObservable(const observable: IObservable);
+procedure TBinding.InitSource(const observable: IObservable);
 begin
-  fObservable := TDependentObservable.Create(
+  fSource := TDependentObservable.Create(
     InitGetValue(observable),
     procedure(const value: TValue)
     begin
@@ -160,19 +160,23 @@ begin
     end);
 end;
 
+procedure TBinding.InitTarget;
+begin
+end;
+
 {$ENDREGION}
 
 
 {$REGION 'TBinding<T>'}
 
-constructor TBinding<T>.Create(const component: T; const observable: IObservable);
+constructor TBinding<T>.Create(const target: T; const source: IObservable);
 begin
-  inherited Create(component, observable);
+  inherited Create(target, source);
 end;
 
-procedure TBinding<T>.SetComponent(const component: TComponent);
+function TBinding<T>.GetTarget: T;
 begin
-  fComponent := T(component);
+  Result := T(inherited Target);
 end;
 
 {$ENDREGION}
@@ -180,16 +184,16 @@ end;
 
 {$REGION 'TComponentBinding'}
 
-constructor TComponentBinding.Create(const component: TComponent;
-  const observable: IObservable; const propertyName: string);
+constructor TComponentBinding.Create(const target: TComponent;
+  const source: IObservable; const propertyName: string);
 begin
   // hardcode property extension for now, build dynamic system later
-  if (component is TControl) and SameText(propertyName, 'Disabled') then
+  if (target is TControl) and SameText(propertyName, 'Disabled') then
     fProperty := ctx.GetType(TypeInfo(TControlHelper)).GetProperty(propertyName)
   else
-    fProperty := ctx.GetType(component.ClassInfo).GetProperty(propertyName);
+    fProperty := ctx.GetType(target.ClassInfo).GetProperty(propertyName);
   Assert(Assigned(fProperty));
-  inherited Create(component, observable);
+  inherited Create(target, source);
 end;
 
 function TComponentBinding.InitGetValue(
@@ -203,9 +207,9 @@ begin
       v := observable.Value;
       // some hardcoded custom value conversion for now
       if (fProperty.PropertyType.Handle = TypeInfo(Boolean)) and v.IsObject then
-        fProperty.SetValue(fComponent, v.AsObject <> nil)
+        fProperty.SetValue(Target, v.AsObject <> nil)
       else
-        fProperty.SetValue(fComponent, v);
+        fProperty.SetValue(Target, v);
     end;
 end;
 
@@ -216,17 +220,17 @@ end;
 
 procedure TButtonBinding.HandleClick(Sender: TObject);
 begin
-  fObservable.Value;
+  Source.Value;
 end;
 
-procedure TButtonBinding.InitComponent;
+procedure TButtonBinding.InitTarget;
 begin
-  fComponent.OnClick := HandleClick;
+  Target.OnClick := HandleClick;
 end;
 
-procedure TButtonBinding.InitObservable(const observable: IObservable);
+procedure TButtonBinding.InitSource(const observable: IObservable);
 begin
-  fObservable := observable;
+  fSource := observable;
 end;
 
 {$ENDREGION}
@@ -236,12 +240,12 @@ end;
 
 procedure TEditBinding.HandleChange(Sender: TObject);
 begin
-  fObservable.Value := fComponent.Text;
+  Source.Value := Target.Text;
 end;
 
-procedure TEditBinding.InitComponent;
+procedure TEditBinding.InitTarget;
 begin
-  fComponent.OnChange := HandleChange;
+  Target.OnChange := HandleChange;
 end;
 
 function TEditBinding.InitGetValue(const observable: IObservable): TFunc<TValue>;
@@ -249,7 +253,7 @@ begin
   Result :=
     function: TValue
     begin
-      fComponent.Text := observable.Value.ToString;
+      Target.Text := observable.Value.ToString;
     end;
 end;
 
@@ -262,21 +266,21 @@ procedure TComboBoxBinding.HandleChange(Sender: TObject);
 var
   o: TObject;
 begin
-  if fComponent.ItemIndex = -1 then
-    fObservable.Value := nil
+  if Target.ItemIndex = -1 then
+    Source.Value := nil
   else
   begin
-    o := fComponent.Items.Objects[fComponent.ItemIndex];
+    o := Target.Items.Objects[Target.ItemIndex];
     if o = nil then
-      fObservable.Value := fComponent.Items[fComponent.ItemIndex]
+      Source.Value := Target.Items[Target.ItemIndex]
     else
-      fObservable.Value := o;
+      Source.Value := o;
   end;
 end;
 
-procedure TComboBoxBinding.InitComponent;
+procedure TComboBoxBinding.InitTarget;
 begin
-  fComponent.OnChange := HandleChange;
+  Target.OnChange := HandleChange;
 end;
 
 function TComboBoxBinding.InitGetValue(const observable: IObservable): TFunc<TValue>;
@@ -288,9 +292,9 @@ begin
     begin
       value := observable.Value;
       if value.IsObject then
-        fComponent.ItemIndex := fComponent.Items.IndexOfObject(value.AsObject)
+        Target.ItemIndex := Target.Items.IndexOfObject(value.AsObject)
       else
-        fComponent.ItemIndex := fComponent.Items.IndexOf(value.ToString);
+        Target.ItemIndex := Target.Items.IndexOf(value.ToString);
     end;
 end;
 
@@ -310,9 +314,9 @@ begin
         Exit;
       v := observable.Value;
       if v.IsEmpty then
-        fComponent.Caption := ''
+        Target.Caption := ''
       else
-        fComponent.Caption := v.ToString;
+        Target.Caption := v.ToString;
     end;
 end;
 
